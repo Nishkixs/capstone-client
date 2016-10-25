48b5b0a0e5a7e0acb36c72252fd1afa500998a6f
nishkixs <nishon.radhakrishnan@gmail.com>
2016-10-25 10:52:41 -0400

    Firebase Test

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -5,9 +5,9 @@ const addHandlers = () => {
   $('.instruction2-page').hide(0);
   $('.home-page').fadeOut(6000,function(){
   $('.instruction-page').show();
-  $('.instruction-page').fadeOut(4000,function(){
-  $('.instruction2-page').show();
-  });
+  // $('.instruction-page').fadeOut(4000,function(){
+  // $('.instruction2-page').show();
+
 });
 
 };
